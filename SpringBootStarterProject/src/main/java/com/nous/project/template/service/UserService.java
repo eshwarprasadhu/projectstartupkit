b7835777e93db295b10934b50543bc82c498b77a
sachinsfirst <sachinsfirst@mail.com>
2020-01-17 16:21:30 +0530

    added the Caching functionality

--- SpringBootStarterProject/src/main/java/com/nous/project/template/service/UserService.java
+++ SpringBootStarterProject/src/main/java/com/nous/project/template/service/UserService.java
@@ -1,23 +1,23 @@
 package com.nous.project.template.service;
 
-import com.nous.project.template.domain.User;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
-/**
- * Created by sahan on 4/9/2016.
- */
+import com.nous.project.template.domain.User;
+
 @Service
 @Transactional
 public class UserService {
 
-    private static List<User> users = new ArrayList<>();
+	private static List<User> users = new ArrayList<>();
 
-    static {
-        users.add(new User("user1@gmail.com", "user1"));
-        users.add(new User("user2@gmail.com", "user2"));
-    }
+	static {
+		users.add(new User("user1@gmail.com", "user1"));
+		users.add(new User("user2@gmail.com", "user2"));
+	}
 
 }
