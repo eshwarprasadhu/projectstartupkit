1e312d949dff1c041ebe2f9554937c64a7a37ac0
Ramakrishna756 <59860069+Ramakrishna756@users.noreply.github.com>
2020-01-20 12:35:59 +0530

    Update ShoppingCartController.java
    Logging implimetation

--- SpringBootStarterProject/src/main/java/com/nous/project/template/controller/ShoppingCartController.java
+++ SpringBootStarterProject/src/main/java/com/nous/project/template/controller/ShoppingCartController.java
@@ -1,5 +1,7 @@
 package com.nous.project.template.controller;
 
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
 
+    private static final Logger log = (Logger) LoggerFactory.getLogger(ShoppingCartController.class);
+
+    public void logerMethod() {
+       log.info("Hello world");
+    }
 }
