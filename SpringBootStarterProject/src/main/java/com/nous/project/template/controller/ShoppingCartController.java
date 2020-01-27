3be90e371bb78000da6a978006c79fc9667e72c6
Ramakrishna756 <59860069+Ramakrishna756@users.noreply.github.com>
2020-01-27 12:18:46 +0530

    Changed the log config

--- SpringBootStarterProject/src/main/java/com/nous/project/template/controller/ShoppingCartController.java
+++ SpringBootStarterProject/src/main/java/com/nous/project/template/controller/ShoppingCartController.java
@@ -1,7 +1,5 @@
 package com.nous.project.template.controller;
 
-import org.slf4j.LoggerFactory;
-import ch.qos.logback.classic.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
 
-    private static final Logger log = (Logger) LoggerFactory.getLogger(ShoppingCartController.class);
 
-    public void logerMethod() {
-       log.info("Hello world");
-    }
 }
