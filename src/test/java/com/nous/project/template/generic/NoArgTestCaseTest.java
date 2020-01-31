f93565952f72dfbb2e7988aa71f496fd68708452
RakshithaHM <50227058+RakshithaHM@users.noreply.github.com>
2020-01-31 18:31:25 +0530

    Committing to branch

--- src/test/java/com/nous/project/template/generic/NoArgTestCaseTest.java
+++
@@ -1,9 +0,0 @@
-//package com.nous.project.template.generic;
-//
-//import junit.framework.TestCase;
-//
-//public class NoArgTestCaseTest extends TestCase {
-//	
-//    public void testNothing() { 
-//    }
-//}
