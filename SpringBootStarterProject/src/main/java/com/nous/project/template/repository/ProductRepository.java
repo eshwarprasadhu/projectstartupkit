efa5cfcfbadf1284640bd33d26130ab7553df2af
RakshithaHM <50227058+RakshithaHM@users.noreply.github.com>
2020-02-03 11:39:56 +0530

    Adding files

--- SpringBootStarterProject/src/main/java/com/nous/project/template/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nous.project.template.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nous.project.template.domain.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
--- SpringBootStarterProject/src/test/java/com/nous/project/template/controller/Test.java
+++
@@ -1,6 +0,0 @@
-package com.nous.project.template.controller;
-
-public class Test {
-	// Sample class
-
-}
